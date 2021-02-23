e708d36fd28217bcd7d9df1ca691f2e8b0c81f5e
Wesley Maciel <wesley@meuti.com.br>
2021-02-23 17:36:28 -0300

    create project

---
+++ src/main/java/net/superbid/backenddeveloper/TesteWesleyMacielApplication.java
@@ -0,0 +1,12 @@
+package net.superbid.backenddeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteWesleyMacielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteWesleyMacielApplication.class, args);
+	}
+}
---
+++ src/test/java/net/superbid/backenddeveloper/TesteWesleyMacielApplicationTests.java
@@ -0,0 +1,13 @@
+package net.superbid.backenddeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteWesleyMacielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
