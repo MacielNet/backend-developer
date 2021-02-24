b096805e2fa77b49e9ef2c787f075ae7bf2be71b
Wesley Maciel <wesley@meuti.com.br>
2021-02-24 00:22:59 -0300

    Created class Post and tested REST

---
+++ src/main/java/net/superbid/backenddeveloper/domain/Post.java
@@ -0,0 +1,95 @@
+package net.superbid.backenddeveloper.domain;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Post implements Serializable {
+	/**
+	 * Class Post 
+	 * author: Wesley Maciel 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	private Timestamp created_at;
+	private Timestamp updated_at;
+	
+	public Post() {
+	}
+
+	public Post(Integer id, String titulo, String descricao, Timestamp created_at, Timestamp updated_at) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Timestamp getCreate_at() {
+		return created_at;
+	}
+
+	public void setCreate_at(Timestamp create_at) {
+		this.created_at = create_at;
+	}
+
+	public Timestamp getUpdate_at() {
+		return updated_at;
+	}
+
+	public void setUpdate_at(Timestamp update_at) {
+		this.updated_at = update_at;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/net/superbid/backenddeveloper/resources/PostResource.java
@@ -0,0 +1,32 @@
+package net.superbid.backenddeveloper.resources;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.superbid.backenddeveloper.domain.Post;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Post> listar() {
+		
+		Post p1 = new Post(null, "Post#001 - Primeiro Post", "Olá, este é o primeiro post da API.", 
+						   new Timestamp(System.currentTimeMillis()),null); 
+		Post p2 = new Post(null, "Post#002 - Segundo Post", "Então, aqui temos outro post!", 
+						   new Timestamp(System.currentTimeMillis()),null);
+		
+		List<Post> lista = new ArrayList<>();
+		lista.add(p1);
+		lista.add(p2);
+		
+		return lista;
+	}
+	
+}
